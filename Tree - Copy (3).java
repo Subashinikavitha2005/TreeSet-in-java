b67bbc0f7c770756e8ae4140732397e046581343
Subashinikavitha2005 <subashini7496@gmail.com>
2024-10-16 19:59:37 +0530

    Add files via upload

---
+++ Tree - Copy (3).java
@@ -0,0 +1,10 @@
+import java.util.TreeSet;
+public class Tree{
+    public static void main(String[] args){
+        TreeSet <String> obj=new TreeSet<>();
+        obj.add("tom");
+        obj.add("patlu");
+        obj.add("motu");
+        System.out.println(obj);
+    }
+}
